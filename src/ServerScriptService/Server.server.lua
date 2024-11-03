8b4b17b5bc5118780f16f4073849dcfbfb4f37f1
DarthSharkie <darthsharkie@gmail.com>
2024-11-03 01:16:12 -0800

    Update todo list

--- src/ServerScriptService/Server.server.lua
+++ src/ServerScriptService/Server.server.lua
@@ -49,3 +49,25 @@ end)
 
 -- TODO: Figure out if there's a better way to have LB connect to Catapult launch
 LeaderboardService:init()
+
+--[[  TODO List
+0. Reset target event hookup (feature parity)
+1. Allow for multiple spawns
+2. Prevent overlapping catapult fire (4-6 per server to allow for enough range of motion)
+   - will need to limit catapult rotation
+   - will need to limit target platform theta
+3. Detect targets knocked over (if they're still standing, it doesn't count)
+4. Save number of launches, number of targets "destroyed" to cloud
+5. Consider badges/achievements for number of launches, number of targets
+6. Figure out how to fully-manage via Rojo... model export from studio didn't work
+7. Think about progression: is it multiple cats?  more powerful cat?  more sophisticated targets?  Skill tree?
+   - new projectile?  bola? explosive? multi-ball?
+   - new cats?  trebuchet?  ballista?
+   - earn currency?
+8. Monetization possible?  What x2 type things, or time savers would people pay for?
+   - Auto-load
+   - Earning multiplier
+   - Buy currency pack(s)
+   - Auto-target?
+   - Auto-fire?
+]]
